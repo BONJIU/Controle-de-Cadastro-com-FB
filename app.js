0fe70256806df825b4b83f58e41d98aa86598ccf
BONJIU <santos.bongiovani@gmail.com>
2024-10-25 18:09:06 -0300

    Atualização

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const post = require("./models/post")
 const { initializeApp, cert } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
-const serviceAccount = require('./webii-1a74e-firebase-adminsdk-if5ht-e952c7f91d.json');
+const serviceAccount = require('./webii-1a74e-firebase-adminsdk-if5ht-1e8f70f277.json');
 
 
 initializeApp({
@@ -13,19 +13,16 @@ initializeApp({
      
 })
 
-
 const db = getFirestore()
 
+app.engine("handlebars", handlebars({
+  defaultLayout: "main",
+  helpers: {
+    eq: (v1, v2) => v1 === v2
+  }
+}));
 
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-const Handlebars = require("express-handlebars").create().handlebars;
-
-Handlebars.registerHelper("eq", function (v1, v2) {
-  return v1 === v2;
-});
-
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
@@ -38,8 +35,10 @@ app.post("/cadastrar", function(req, res){
     observacao: req.body.observacao
  }).then(function(){
     console.log("Dados cadastrados com sucesso!")
+    res.redirect("/consultar");
  })
 })
+
 app.get("/", function(req, res){
     res.render("primeira_pagina")
 })
@@ -51,65 +50,52 @@ app.get("/confirmar", function(req, res){
 })
 
 
-app.get("/consultar", function(req, res){
-    var posts = []
-    db.collection('Clientes').get().then(
-      function(snapshot){
-        snapshot.forEach(function(doc){
-          const data = doc.data()
-          data.id = doc.id
-          posts.push(data)
-        })
-        res.render("segunda_pagina",{posts: posts})
-      }
-    )
+app.get("/consultar", function (req, res) {
+  const posts = [];
+  db.collection('Clientes').get().then(snapshot => {
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      data.id = doc.id;
+      posts.push(data);
+    });
+    res.render("segunda_pagina", { posts });
   })
+});
 
-app.post("/atualizar", function(req,res){
-    post.update({
-        nome: req.body.nome,
-        telefone: req.body.telefone,
-        origem: req.body.origem,
-        data_contato: req.body.data_contato,
-        observacao: req.body.observacao
-    }).then(
-        function(){
-            console.log('Dados atualizados com sucesso!')
-            res.render('/consultar')
-        }
-    )
-})
-
+app.post("/atualizar", function (req, res) {
+  const id = req.body.id;
+  console.log("ID recebido:", id);
+  db.collection("Clientes").doc(id).update({
+    nome: req.body.nome,
+    telefone: req.body.telefone,
+    origem: req.body.origem,
+    data_contato: req.body.data_contato,
+    observacao: req.body.observacao
+  })
+    .then(() => {
+      console.log("Dados atualizados com sucesso!");
+      res.redirect("/consultar");
+    })
+});
 
 app.get("/excluir/:id", function (req, res) {
-    const id = req.params.id;
-    console.log("ID recebido:", id);
-    db.collection("Clientes").doc(id).get().then((doc) => {
-        if (doc.exists) {
-          const data = doc.data();
-          data.id = doc.id;
-          res.render("quarta_pagina", { posts: [data] });
-        }
-      });
+  const id = req.params.id;
+  var result = db.collection('Clientes').doc(id).delete().then(function () {
+      console.log('Documento excluído com sucesso!');
+      res.redirect('/consultar');
   });
-
-app.post("/excluir", function (req, res) {
-    const id = req.body.id;
-    db.collection("Clientes").doc(id).delete().then(function(){
-        console.log("Dados excluídos com sucesso!");
-        res.redirect("/consultar");
-      });
-  });
-  
-
-app.get("/editar/:id", function(req, res){
-        post.findAll({where: {'id' : req.params.id}}).then(
-            function(posts){
-                res.render("terceira_pagina", {posts})
-                console.log(posts)
-            }
-        )
-    })
+});
+app.get("/editar/:id", function (req, res) {
+  const id = req.params.id;
+  db.collection("Clientes").doc(id).get()
+    .then(doc => {
+      if (doc.exists) {
+        const post = doc.data();
+        post.id = doc.id;
+        res.render("terceira_pagina", { posts: [post] });
+      }
+    });
+});
 
 app.listen(8081, function(){
     console.log("Servidor funfando")
